5d81d4201bbe56720bd8263970263a81f5406626
Munir1996 <32868140+Munir1996@users.noreply.github.com>
2017-11-10 14:13:12 +0000

    Added Files by upoload
    fix validation for the movement of pieces

---
+++ Pieces.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checkers
+{
+    public class Pieces
+    {
+        private const string playerSymbolB = "B";//player 1 character
+        private const string playerSymbolW = "W";//player 2 character
+        public const string space = " ";//spaces
+        public static string[,] piece; //holds characters
+
+        public Pieces()//constructor
+        {
+            piece = new string[Checkerboard.size, Checkerboard.size];
+            StartPieces();//populate array
+        }
+
+        private void StartPieces()//populate the array with B and W or blanks
+        {
+            for (int row = 0; row < Checkerboard.size; row++)
+            {
+                for(int column = 0; column < Checkerboard.size; column++)
+                {
+                    if (row == 0 || row == 1 || row == 2)
+                    {
+                        piece[row, column] = playerSymbolB;//populate the first three row
+                        BlankList();
+                    }
+                    else if(row == 5 || row == 6 || row == 7)
+                    {
+                        piece[row, column] = playerSymbolW;//populate the last three rows 
+                        BlankList();
+                    }
+                    else
+                    {
+                        piece[row, column] = space;
+                    }
+                }
+            }
+        }//end method
+
+        private void BlankList()
+        {
+            //playwer 2 side
+            piece[5, 1] = space;//draw blank
+            piece[5, 3] = space;
+            piece[5, 5] = space;
+            piece[5, 7] = space;
+            piece[6, 0] = space;
+            piece[6, 2] = space;
+            piece[6, 4] = space;
+            piece[6, 6] = space;
+            piece[7, 1] = space;
+            piece[7, 3] = space;
+            piece[7, 5] = space;
+            piece[7, 7] = space;
+            
+
+            //player 1 side
+            piece[0, 0] = space;
+            piece[0, 2] = space;
+            piece[0, 4] = space;
+            piece[0, 6] = space;
+            piece[1, 1] = space;
+            piece[1, 3] = space;
+            piece[1, 5] = space;
+            piece[1, 7] = space;
+            piece[2, 0] = space;
+            piece[2, 2] = space;
+            piece[2, 4] = space;
+            piece[2, 6] = space;
+
+            //empty
+            piece[3, 1] = space;
+            piece[3, 3] = space;
+            piece[3, 5] = space;
+            piece[3, 7] = space;
+            piece[4, 0] = space;
+            piece[4, 2] = space;
+            piece[4, 4] = space;
+            piece[4, 6] = space;
+        }
+    }//end class
+}//end namespace
---
+++ Player.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checkers
+{
+    public class Player : Pieces
+    {
+        private int targetX;//coordinates for which pieces you want to take on the x axis
+        private int targetY;//coordinates for which pieces you want to take on the y axis
+        private int destinationX;//coordinates for which pieces you want to place on the x axis
+        private int destinationY;//coordinates for which pieces you want to place on the x axi
+        
+        public Player()//constructor
+        {
+            targetX = 0;
+            targetY = 0;
+            destinationX = 0;
+            destinationY = 0;
+            Exit = false;
+        }
+
+        public bool Exit { get; set; }
+
+        public void Move()
+        {
+            GetInput();//call
+
+            if(!Exit == true)
+            ReplacePieces();//call
+            
+        }
+
+        private void GetInput()//get input and validate
+        {
+            if (!Exit)
+            {
+                Console.WriteLine("Enter targets X axis");
+                Exit = ValidateInput(int.TryParse(Console.ReadLine(), out targetX));
+            }
+
+            if (!Exit)//if it pass the validation move to next validation
+            {
+                Console.WriteLine("Enter targets Y axis");
+                Exit = ValidateInput(int.TryParse(Console.ReadLine(), out targetY));
+            }
+
+            if (!Exit)//if it pass the validation move to next validation
+            {
+                Console.WriteLine("Enter destination X axis");
+                Exit = ValidateInput(int.TryParse(Console.ReadLine(), out destinationX));
+            }
+
+            if (!Exit)//if it pass the validation move to next validation
+            {
+                Console.WriteLine("Enter destination Y axis");
+                Exit = ValidateInput(int.TryParse(Console.ReadLine(), out destinationY));
+            }
+
+            if(destinationY != targetY + 1)//if false then stop
+            {
+                Exit = true;
+                return;
+            }
+
+            if(destinationX != targetX + 1 && destinationX != targetX - 1)
+            {
+                //check if to take a piece if
+                Exit = true;
+                return;
+                
+            }
+
+            if(piece[destinationY,destinationX] != space)
+            {
+                Exit = true;
+                return;
+            }
+        }//end method
+
+        private void ReplacePieces()
+        {
+
+            piece[destinationY, destinationX] = piece[targetY, targetX];//change piece
+            piece[targetY, targetX] = space;
+        }
+
+        private bool ValidateInput(bool pass)
+        {
+            bool error = false;
+
+            if (!pass)
+            {
+                error = true;
+            }
+            else if (targetX < 0 || targetY < 0 | destinationX < 0 | destinationY < 0)//exit if under 0
+            {
+                error = true;
+            }
+            else if (targetX > Checkerboard.size - 1 || targetY > Checkerboard.size - 1 || destinationX > Checkerboard.size - 1 || destinationY > Checkerboard.size - 1)//exit if over 7
+            {
+                error = true;
+            }
+
+            
+
+            if (error)
+            {
+                Console.WriteLine("invalid input. exiting program now! ");
+            }
+
+            return error;
+        }
+    }//end class
+}//end namespace
